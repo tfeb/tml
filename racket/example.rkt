ed4e5bc553137e2809400e02eea662f54e6faee1
Tim Bradshaw <tfb@cley.com>
2021-11-15 13:15:45 +0000

    Add example for racket version

---
+++ racket/example.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require "tml.rkt")
+
+(define primitives
+  (augment-environment*
+   empty-environment
+   'eq? eq?
+   'void void
+   'false #f
+   'true #t))
+
+(define-environment (conses primitives)
+  (cons (λ (a b)
+          (λ (m)
+            (m a b))))
+  (car (λ (c)
+         (c (λ (a b) a))))
+  (cdr (λ (c)
+         (c (λ (a b) b))))
+  (nil '())
+  (null? (λ (a) (eq? a nil))))
+
+(define-macro (begin . forms)
+  `((λ () ,@forms)))
+
+(define-macro (when form . forms)
+  `(if ,form (begin ,@forms) (void)))
+
+(parameterize ([trace-expansion #t])
+  (expand-macros '(when 1 2 3)))
+
+(evaluate '(null? (car (cons 1 2))) conses)
